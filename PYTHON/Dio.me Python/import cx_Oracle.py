817430c5df14b279fcd24a5a207d726c83b9e4e9
PedroSanpei <prubio05@gmail.com>
2022-10-04 17:25:11 -0300

    TESTE DE INSERÇÃO PANDA

--- PYTHON/Dio.me Python/import cx_Oracle.py
+++ PYTHON/Dio.me Python/import cx_Oracle.py
@@ -1,5 +1,6 @@
+from pickle import NONE
 import cx_Oracle
-import csv
+import pandas as pd
 
 cx_Oracle.init_oracle_client(lib_dir=r"C:\oracle\instantclient_19_16")
 dsn_tns = cx_Oracle.makedsn('PEDRO-DESK', '1521', service_name='XE') # if needed, place an 'r' before any parameter in order to address special characters such as '\'.
@@ -7,23 +8,16 @@ conn = cx_Oracle.connect (user=r'pedro', password='pedro123', dsn=dsn_tns) # if 
 print('A Versão é:', conn.version)
 cursor = conn.cursor()
 cursor.execute("""
-        SELECT first_name, last_name
+        SELECT employee_Id,first_name, last_name
         FROM hr.employees
-        WHERE department_id = :did AND employee_id > :eid""",
+        WHERE department_id = :did AND employee_id > :eid AND employee_Id = :empid""",
         did = 50,
-        eid = 190)
-for fname, lname in cursor:
-    resultado = fname + ' ' +  lname
-    print(resultado)
-    with open('LOG.csv', 'w', encoding='UTF8', newline='') as f:
-        
-        writer = csv.writer(f)
-        #header = ['fname','lname']
-        data = [resultado]
-        
-        # write the header
-        #  writer.writerow(header)
-
-            # write the data
-        writer.writerow(data)
+        eid = 190,
+        empid = 195
+        )
+for employeeid, fname, lname in cursor:
+    print(employeeid,fname,lname)
+        # df = pd.DataFrame([[fname, lname]], columns=["fname", "lname"])  
+        # with pd.ExcelWriter("path_to_file.xlsx") as writer:
+        #     df.to_excel(writer) 
 conn.close()
