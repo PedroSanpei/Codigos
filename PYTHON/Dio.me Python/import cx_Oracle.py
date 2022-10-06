8350861e2aab2a3417a25d487bc1410b09535a6d
PedroSanpei <prubio05@gmail.com>
2022-10-05 22:20:16 -0300

    SUBINDO INTENSIVÃO HASHTAG TREINAMENTOS PYTHON

--- PYTHON/Dio.me Python/import cx_Oracle.py
+++ PYTHON/Dio.me Python/import cx_Oracle.py
@@ -1,4 +1,6 @@
 from pickle import NONE
+from this import d
+from unittest import result
 import cx_Oracle
 import pandas as pd
 
@@ -7,17 +9,9 @@ dsn_tns = cx_Oracle.makedsn('PEDRO-DESK', '1521', service_name='XE') # if needed
 conn = cx_Oracle.connect (user=r'pedro', password='pedro123', dsn=dsn_tns) # if needed, place an 'r' before any parameter in order to address special characters such as '\'. For example, if your user name contains '\', you'll need to place 'r' before the user name: user=r'User Name'
 print('A Versão é:', conn.version)
 cursor = conn.cursor()
-cursor.execute("""
-        SELECT employee_Id,first_name, last_name
-        FROM hr.employees
-        WHERE department_id = :did AND employee_id > :eid AND employee_Id = :empid""",
-        did = 50,
-        eid = 190,
-        empid = 195
-        )
-for employeeid, fname, lname in cursor:
-    print(employeeid,fname,lname)
-        # df = pd.DataFrame([[fname, lname]], columns=["fname", "lname"])  
-        # with pd.ExcelWriter("path_to_file.xlsx") as writer:
-        #     df.to_excel(writer) 
+cursor.execute('SELECT * FROM hr.employees') 
+for resultado in cursor:
+        print(resultado)
+        dataDf = pd.DataFrame(columns=["EMPLOYEE_ID", "FIRST_NAME", "LAST_NAME", "EMAIL", "PHONE_NUMBER", "HIRE_DATE", "JOB_ID", "SALARY", "COMMISSION_PCT", "MANAGER_ID", "DEPARTMENT_ID" ],
+                                        data = resultado)
 conn.close()
