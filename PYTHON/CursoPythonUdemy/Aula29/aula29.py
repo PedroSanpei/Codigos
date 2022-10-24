8d23087eb0b7b35a79d72f54a798b5db3911a627
PedroSanpei <prubio05@gmail.com>
2022-10-24 18:57:52 -0300

    Cursinho DIO

---
+++ PYTHON/CursoPythonUdemy/Aula29/aula29.py
@@ -0,0 +1,8 @@
+valor = False
+
+if valor:
+    # ... = Elipsis significa que está segurando o código para escrever depois
+    # pass é a mesma coisa porém do jeito correto.
+    ...
+else:
+    print('Tchau')
