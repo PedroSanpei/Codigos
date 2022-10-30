0bb02c03c54e74fe7d7f62a816201f84322bf1f2
PedroSanpei <prubio05@gmail.com>
2022-10-30 00:10:32 -0300

    PROJETO TESTE LOGIN E SENHA

--- PYTHON/CursoPythonUdemy/Login_Senha.py
+++ PYTHON/CursoPythonUdemy/Login_Senha.py
@@ -1,18 +1,24 @@
 """
 OBJETIVO CRIAR UM PROGRAMA DE LOGIN E SENHA QUE BUSCA A INFORMAÇÃO DE UM BANCO DE DADOS
 """
+
+
 def login_senha():
     while True:
+        # IMPORTAR A BIBLIOTECA PANDA
         import pandas as pd
+        # LER O EXCEL
         planilha = pd.read_excel("C:\\WORKSPACE\\Codigos\\PYTHON\\CursoPythonUdemy\\Dados.xlsx",
                                  sheet_name='dados_user')
-
+        # INPUTS DE LOGIN E SENHA
         login = input('Digite seu Login: ')
         password = int(input('Digite sua Senha: '))
 
+        # LISTA DE CHECAGEM DE LOGIN E SENHA COM BASE NA PLANILHA
         check_login = set(list(planilha['LOGIN']))
         check_senha = set(list(planilha['SENHA']))
 
+        # CONDIÇÕES DE VALIDAÇÃO
         if login in check_login and password in check_senha:
             print(f'Seja Bem Vindo {login}')
             break
