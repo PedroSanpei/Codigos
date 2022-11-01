3ec0ae924b5f9d2a2e98a8f182681915311e7504
PedroSanpei <prubio05@gmail.com>
2022-11-01 17:25:24 -0300

    Atualização:
    * LOGIN
    * CADASTRO
    * ENVIO DE E-MAIL

--- PYTHON/CursoPythonUdemy/Login_Senha_Oracle.py
+++ PYTHON/CursoPythonUdemy/Login_Senha_Oracle.py
@@ -9,7 +9,6 @@ while True:
     login_page = int(input("""1- Logar, 2- Cadastar: """))
     # 1.1 LOGAR-SE
     if login_page == 1:
-
         # 1.1.1 INPUTS DE LOGIN E SENHA
         login = input('Digite seu Login: ')
         password = int(input('Digite sua Senha: '))
@@ -25,6 +24,7 @@ while True:
         # 1.1.5 MONTANDO CURSOR
         cursor1 = conn.cursor()
         cursor2 = conn.cursor()
+        cursor3 = conn.cursor()
 
         # 1.1.6 EXECUTANDO O CURSOR COM UM SELECT
         check_login = cursor1.execute(f"SELECT nvl(LOGIN,'') FROM LOGIN_SENHA WHERE LOGIN = '{login}'")
@@ -43,7 +43,7 @@ while True:
             if cadastrar_se == 'n':
                 break
             elif cadastrar_se == 's':
-                login_page2 = int(input("""Digite 2- Para Cadastrar-se """))
+                pass
 
         # 1.1.8.2 CONDIÇÃO 2
         elif login in data1 and password != data2:
@@ -60,12 +60,46 @@ while True:
     # 1.2 CADASTRAR-SE
 
     elif login_page == 2:
-        login_cadast = input('Digite nome.sobrenome: ')
-        # # passw_cadast = input('Digite uma senha de até 3 digitos: ')
-        # import openpyxl
-        # from openpyxl import load_workbook
-        # arquivo_excel = openpyxl.load_workbook("C:\\WORKSPACE\\Codigos\\PYTHON\\CursoPythonUdemy\\Dados.xlsx")
-        # planilha1 = arquivo_excel.active
-        #
-        # max_linha = planilha1.max_row
-        # for i in range(1, max_linha):
+        v_nome = input('Digite o seu nome: ').lower()
+        v_sobrenome = input('Digite o seu sobrenome: ').lower()
+        v_password = input('Digite sua senha até 10 digítos: ')
+        v_email = input('Informe seu e-mail: ')
+
+        login_cadastrado = v_nome + '.' + v_sobrenome
+
+        tam_log_pad = 20
+        tam_pass_pad = 10
+
+        if len(login_cadastrado) > tam_log_pad:
+            print(f'Login não pode ser maior que 20 caracteres. O login digitado tem {len(login_cadastrado)} caracteres')
+        elif len(v_password) > tam_pass_pad:
+            print(f'Senha não pode ser maior que 10 dígitos. A senha digitada tem {len(v_password)} dígitos')
+        else:
+            print('Login e Senha criados com sucesso!')
+            import win32com.client as win32
+
+            # criar a integração com o outlook
+            outlook = win32.Dispatch('outlook.application')
+
+            # criar um email
+            email = outlook.CreateItem(0)
+
+            # configurar as informações do seu e-mail
+            email.To = f"{v_email};"
+            email.Subject = "Cadastro do Login e Senha"
+            email.HTMLBody = f"""
+            <p>Olá {v_nome} {v_sobrenome}, aqui é o da tela de Login da ativadade. Segue informações abaixo:</p>
+
+            <p>O Seu Login é: {login_cadastrado}</p>
+            <p>A Sua Senha é: {v_password}</p>
+
+            <p>Abs,</p>
+            <p>Código Python</p>
+            """
+
+            # anexo = "C://Users/joaop/Downloads/arquivo.xlsx"
+            # email.Attachments.Add(anexo)
+
+            email.Send()
+            print("Email Enviado")
+            break
