994baad9e814db02b3666e9ad75d448ba8eade97
Pedro Sanpei <pedro_sanpei@kawasakibrasil.com>
2022-11-04 15:36:14 -0300

    Aula 38 Curso Python Udemy - Listas

---
+++ PYTHON/CursoPythonUdemy/Aula38/Aula38_Listas.py
@@ -0,0 +1,22 @@
+"""
+LISTAS EM PYTHON
+FATIAMENTO
+APPEND, INSERT, POP, DEL, CLEAR, EXTEND, +, MIN, MAX RANGE
+"""
+
+
+#INDEX +    0    1       2    3    4         5     6
+lista1 =   ['A', 'Bife', 'C', 'D', 'Escuela', 10.5, 10]
+#INDEX -    7    6       5    4    3         2     1
+
+lista2 =   [1, 2, 2.1, 3, 4]
+
+print(lista1,lista2)
+
+# FORMATAÇÃO DE UM INDEX DA LISTA
+lista1[5] = 'Qualquer Coisa'
+
+# FATIAMENTO DE UMA LISTA
+print(lista1[0], lista1[5])
+
+
