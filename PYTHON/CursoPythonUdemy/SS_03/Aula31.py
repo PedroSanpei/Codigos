3a86f4f156649cd0c7bb8ebb766fec5c5799f24b
PedroSanpei <prubio05@gmail.com>
2022-11-20 18:45:03 -0300

    MERGE DA AULA 31

--- PYTHON/CursoPythonUdemy/SS_03/Aula31.py
+++ PYTHON/CursoPythonUdemy/SS_03/Aula31.py
@@ -5,8 +5,24 @@ is e is not = é ou não é (Tipo, valor, identidade)
 id = Identidade
 """
 
-v1 = 'a'
+# ID
+"""v1 = 'a'
 v2 = 'a'
 print(id(v1))
 print(id(v2))
-print(id(v1)==id(v2))
+print(id(v1)==id(v2))"""
+
+condicao = False
+passsou_no_if = None
+if condicao:
+    passsou_no_if = True # Flag
+    print('Faço Algo')
+else:
+    print('Não faça algo')
+# print(passsou_no_if, passsou_no_if is None)
+# print(passsou_no_if, passsou_no_if is not None)
+
+if passsou_no_if is None:
+    print('Não Passou no IF')
+if passsou_no_if is not None:
+    print('Passou no IF')
