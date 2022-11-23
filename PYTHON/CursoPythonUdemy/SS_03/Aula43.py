a1a60256eb1ebc3ece55687b0d2055737860a9e7
PedroSanpei <prubio05@gmail.com>
2022-11-23 20:29:21 -0300

    Aulas 43 a 45

---
+++ PYTHON/CursoPythonUdemy/SS_03/Aula43.py
@@ -0,0 +1,9 @@
+texto = 'PythonP'
+lista = ['Pedro','Raul','Otavio']
+
+novo_texto = ''
+
+for letra in texto:
+    novo_texto += f'*{letra}'
+    # print(letra)
+print(novo_texto + '*')
---
+++ PYTHON/CursoPythonUdemy/SS_03/Aula44.py
@@ -0,0 +1,9 @@
+"""
+FOR + RANGE
+Range -> range(start,stop,step)
+"""
+
+numeros = range(0,100,2)
+
+for numero in numeros:
+    print(numero)
